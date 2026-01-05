859f5efb1d12130546ca7f95637c679aeae17649
DENNIS DRECKS <dreckssuave@gmail.com>
2026-01-05 16:19:28 +0300

    final work

--- portfolio.js
+++ portfolio.js
@@ -25,3 +25,23 @@ function openMenu(){
 function closeMenu(){
     openmenu.style.display= "none";
 }
+
+// contact form
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzCI-HD3hXibNmwKn-NjQFlyfMtKTV51evGIiDhCXix-_0h59FtUak1fudXYm74rREj/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('confirm')
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message send successfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+
+        },1000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
